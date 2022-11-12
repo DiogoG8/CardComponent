bd4570ee993e4341d349201c1cdd23a400542ef0
DiogoG8 <110616195+DiogoG8@users.noreply.github.com>
2022-11-12 22:44:55 +0000

    Card component  updated

---
+++ src/App.js
@@ -0,0 +1,15 @@
+import './style.scss';
+import Card from './components/card';
+import React from 'react';
+
+
+function App() {
+  return (
+    <div>
+       <Card/>
+    </div>
+  );
+}
+
+export default App;
+
